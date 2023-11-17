8c4615d1e99371a4af9d25ed8187972f738f0201
Anikribhu <95336760+Anikribhu@users.noreply.github.com>
2023-11-17 10:44:26 +0530

    Dropdown menu added

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ function showSlides() {
    slides[slideIndex - 1].style.display = "block";
    setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
